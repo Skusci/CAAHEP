9bdf7d2c6c6243122018e0ade2bc9e2b0308af8f
Scott McGraw <scott.mcgraw@gatech.edu>
2019-08-02 01:11:10 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -95,9 +95,10 @@ def get_pages():
     """
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--headless');
+    options.add_argument('--disable-gpu')
+    options.add_argument('--disable-site-isolation-trials')
+    options.add_argument('--no-sandbox');
+    
 
     br = webdriver.Chrome(chrome_options=options, executable_path='/usr/local/bin/chromedriver')
     
